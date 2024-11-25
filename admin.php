ade414da8f8f437a3c98500c5ab3e9e1d32504a3
jemszklanki <u20_ernestkosieradzki@zsp1.siedlce.pl>
2024-11-25 21:45:38 +0100

    Przyciski

--- admin.php
+++ admin.php
@@ -10,11 +10,17 @@ if(isset($_SESSION['admin'])){
 ?>
 
 <main class="container mt-4">
-    <button id="users" onclick="getForm(1)">Karty</button>
-    <button id="cards" onclick="getForm(2)">Dodatki</button>
-    <button id="expansions" onclick="getForm(3)">Użytkownicy</button>
-    <div id="ajax-ret">
-        <!-- Tutaj ajax ze skryptu -->
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+            <div class="btn-group mr-2" role="group" aria-label="First group">
+                <button type="button" class="btn btn-primary" id="users" onclick="getForm(1)">Karty</button>
+                <button type="button" class="btn btn-primary" id="cards" onclick="getForm(2)">Dodatki</button>
+                <button type="button" class="btn btn-primary" id="expansions" onclick="getForm(3)">Użytkownicy</button>
+            </div>
+        </div>    
+                <div id="ajax-ret">
+            <!-- Tutaj ajax ze skryptu -->
+        </div>
     </div>
 </main>
 <script src="js/admin_panel.js"></script>
