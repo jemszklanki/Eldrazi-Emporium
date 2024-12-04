ccae74d6249e6a660fa0c4258a84af5172187d1d
jemszklanki <u20_ernestkosieradzki@zsp1.siedlce.pl>
2024-12-04 20:32:55 +0100

    Confirm gotowy

--- confirm.php
+++ confirm.php
@@ -4,22 +4,24 @@ require_once("db.php");
 require_once("navbar.php");
 
     function redirect(){ //funkcja cofająca na index
-        header('Location: index.php')
+       header('Location: index.php');
         exit();
     }
 
     if (!isset($_GET['email']) || !isset($_GET['token'])) {
+        echo"isset";
         redirect();
     } else {
+        $email = $conn->real_escape_string($_GET['email']); // wziecie emailu z linku i spowodowanie że będzie bezpieczny do użycia w zapytaniu sql
+        $token = $conn->real_escape_string($_GET['token']); // token z linku
 
-        $email = $con->real_escape_string($_GET['email']); // wziecie emailu z linku i spowodowanie że będzie bezpieczny do użycia w zapytaniu sql
-        $token = $con->real_escape_string($_GET['token']); // token z linku
-
-        $sql = $con->query("SELECT id FROM users WHERE email='$email' AND token=$token'");
+        $sql = $conn->query("SELECT id FROM users WHERE email='$email' AND token='$token'
+            AND verified=0");
         if($sql->num_rows > 0){
-            $con->query("UPDATE users SET verified=1 AND token=''");
+            $conn->query("UPDATE users SET verified=1, token='' WHERE token='$token' AND email='$email'");
             redirect();
-        } else
+        } else {
             redirect();
+        }
     }
 ?>
