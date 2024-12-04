3242b74f30049f071a3fcb9440b592b97772668e
jemszklanki <u20_ernestkosieradzki@zsp1.siedlce.pl>
2024-12-04 21:27:22 +0100

    login

--- login.php
+++ login.php
@@ -8,27 +8,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $login_input = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = ? OR email = ?";
+    $sql = "SELECT * FROM users WHERE (username = ? OR email = ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss", $login_input, $login_input);
     $stmt->execute();
     $result = $stmt->get_result();
-
+    
     if ($result->num_rows == 1) {
         $user = $result->fetch_assoc();
-        // Sprawdź hasło
-        if (password_verify($password, $user['password'])) {
-            // Wsadź dane do sesji
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['username'] = $user['username'];
-            if ($user['admin'] == true){
-                $_SESSION['admin'] = $user['admin'];
-            }
-            // Przekieruj na index po zalogowaniu
-            header("Location: index.php");
-            exit();
+    
+        // Sprawdź, czy konto jest zweryfikowane
+        if ($user['verified'] == 0) {
+            $error_msg = "Zweryfikuj konto!";
         } else {
-            $error_msg = "Niepoprawne hasło!";
+            // Sprawdź hasło
+            if (password_verify($password, $user['password'])) {
+                // Wsadź dane do sesji
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                if ($user['admin'] == true) {
+                    $_SESSION['admin'] = $user['admin'];
+                }
+                // Przekieruj na index po zalogowaniu
+                header("Location: index.php");
+                exit();
+            } else {
+                $error_msg = "Niepoprawne hasło!";
+            }
         }
     } else {
         $error_msg = "Nie znaleziono użytkownika!";
