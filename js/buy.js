a29a874c2aa9239e554abcc296269d590a6e607b
jemszklanki <u20_ernestkosieradzki@zsp1.siedlce.pl>
2024-12-10 00:35:02 +0100

    Zamówienia dodawanie do tabeli i mail

--- js/buy.js
+++ js/buy.js
@@ -1,10 +1,32 @@
 const comboBox = document.getElementById("ship");
 const street = document.getElementById("stret");
 const number = document.getElementById("numb");
-const siea = document.getElementById("eloKurwa");
+const siea = document.getElementById("fullprice");
 
 comboBox.addEventListener("input", ()=>{
     if(comboBox.value == "3")
+    {
+        street.classList.add("hidden");
+        number.classList.add("hidden");
+        street.required = false;
+        number.required = false;
+    } else {
+        street.classList.remove("hidden");
+        number.classList.remove("hidden");
+        street.required = true;
+        number.required = true;
+    }
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            siea.innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("GET", "queries/calc_delivery.php?id=" + comboBox.value, true);
+    xmlhttp.send();
+})
+
+if(comboBox.value == "3")
     {
         street.classList.add("hidden");
         number.classList.add("hidden");
@@ -20,4 +42,3 @@ comboBox.addEventListener("input", ()=>{
     };
     xmlhttp.open("GET", "queries/calc_delivery.php?id=" + comboBox.value, true);
     xmlhttp.send();
-})
--- queries/calc_delivery.php
+++ queries/calc_delivery.php
@@ -3,6 +3,6 @@
     session_start();
     $shipment_price_query = mysqli_query($conn,"select * from order_shipment where shipment_id='{$_GET['id']}'");
     $result = @mysqli_fetch_array($shipment_price_query);
-    $totalPrice = $cardsPrice + (float)$result['shipment_price'];
-    echo $totalPrice;
+    $_SESSION['totalPrice'] = $_SESSION['cardsPrice'] + (float)$result['shipment_price'];
+    echo 'Cena zamówienia: ' . $_SESSION['totalPrice'] . 'zł';
 ?>
