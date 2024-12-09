ddb542ca81a5579e9b487025631045c6da7acd1c
jestemSzklanka <u20_maksymmnich@zsp1.siedlce.pl>
2024-12-09 04:51:52 +0100

    JS do strony z zakupem

---
+++ js/buy.js
@@ -0,0 +1,11 @@
+const comboBox = document.getElementById("id-twojego-combo-boxa");
+const street = document.getElementById("id-twojego-inputu");
+const number = document.getElementById("id-twojego-inputu");
+
+comboBox.addEventListener("input", ()=>{
+    if(comboBox.value == "wartosc-po-jaka-sprawdzasz")
+    {
+        // street.classList.add("hidden");
+        // street.classList.remove("hidden");
+    }
+})
